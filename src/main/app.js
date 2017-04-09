160dcbc34adba5393a661e6dc2a9f74c8358c645
thefoxbot <krakaka088@gmail.com>
2017-04-09 22:13:27 +0300

    requested stuff

--- src/main/app.js
+++ src/main/app.js
@@ -5,9 +5,23 @@ const data = require("../json/credentials.json");
 bot.login(data);
 
 bot.on('ready', () => {
-    console.log('Logged in as ${client.user.username}!');
+    bot.user.setGame("something")
+    console.log('Logged in as ${bot.user.username}, running on ${bot.guilds.size} servers!'); //client isn't defined in here
+    bot.channels.get("channel id").sendMessage("Started up!") //requested
 });
 
+function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace("@","@ ");
+  else
+      return text;
+}
+
 bot.on('message', msg => {
-    if (msg.content === '')
+    let params = msg.content.split(" ").splice(1);
+    if (msg.content === "!ping") { 
+        msg.reply("Pong! (calculating...)").then(m => {
+            var ping = m.createdTimestamp-msg.createdTimestamp
+            m.edit("Pong! ("+ping+"ms)")
+        })
 })
